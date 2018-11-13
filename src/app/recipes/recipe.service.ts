d50713327b9110415ebd31eefc14d4326f68a77b
Prem Rajah <prajah@ctlondon.com>
2018-11-13 14:25:12 +0000

    finished services, starting routing

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,42 @@
-import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { Recipe } from "./recipe.model";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+
+@Injectable()
 export class RecipeService {
+  public recipeSelected = new EventEmitter<Recipe>();
 
-    public recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+      
+    new Recipe(
+      "Chicken jalfrezi",
+      "This curry is also healthy with two of your 5-a-day and a dose of vitamin C",
+      "https://goo.gl/4VGg8R",
+      [
+          new Ingredient("Onions (1/2)", 1),
+          new Ingredient("Garlic Cloves", 2)
+      ]
+    ),
+    new Recipe(
+      "Barley, chicken & mushroom risotto",
+      "Love risotto? Try this low-fat version, which swaps traditional risotto rice for pearl barley",
+      "https://goo.gl/96dCdP",
+      [
+          new Ingredient("Butter (tbsp)", 1),
+          new Ingredient("Shallots (L)", 2)
+      ]
+    )
+  ];
 
-    private recipes: Recipe[] = [
-        new Recipe('A test recipe', 'this is a test', 'https://placeimg.com/640/480/any'),
-        new Recipe('Another test recipe', 'this is another test', 'https://placeimg.com/200/480/any')
-      ];
-    
-      getRecipes() {
-          return this.recipes.slice();
-      }
+  constructor(private slService: ShoppingListService){}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
 }
